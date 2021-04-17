c2480635073caa2642dfeb789f14cf9caba83be1
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-04-16 21:48:46 -0400

    modifcation

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var entrada = document.getElementById('texto-entrada').innerHTML
 
 function countWords(str) {
-    return str.split(' ').length;
+    return "Cantidad de Palabras: " + str.split(' ').length;
+}
+
+function ListWords(str) {
+    var palabras = str.split(' ')
+    var resultados = document.getElementById('resultados')
+    for(var i = 0; i < palabras.length ; i++){
+        var palabra = palabras[i];
+        
+        var p = document.createElement("p"); 
+        var texto_del_link = document.createTextNode(p); 
+        resultados.appendChild(texto_del_link);
+
+    }
 }
 
 console.log (countWords(entrada))
@@ -11,3 +24,5 @@ var words_quantity = countWords(entrada)
 var texto_del_link = document.createTextNode(words_quantity);
 
 document.getElementById('resultados').appendChild(texto_del_link)
+
+ListWords(entrada)
