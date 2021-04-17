2f988ef81d73f3458e6fe44810d9eab81ec32cb3
Fernando <fp.naldo@gmail.com>
2021-04-16 21:57:31 -0400

    Otros cambios

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var entrada = document.getElementById('texto-entrada').innerHTML
 
 function countWords(str) {
-    return str.split(' ').length;
+    return "Cantidad de palabras: " + str.split(' ').length;
+}
+
+function ListWords(str) {
+    var palabras = str.split(' ')
+    for(var i = 0; i < palabras.length; i++){
+        var palabra = palabras[i]
+        
+        var p = document.createElement("p");
+        var texto_del_link = document.createTextNode();
+        resultados.appendChild(texto_del_link);
+
+        document.getElementById('resultados').appendChild(text_palabra)
+    }
 }
 
 console.log (countWords(entrada))
@@ -10,4 +23,4 @@ var words_quantity = countWords(entrada)
 
 var texto_del_link = document.createTextNode(words_quantity);
 
-document.getElementById('resultados').appendChild(texto_del_link)
+document.getElementById('resultados').appendChild("Cantidad de palabras" + texto_del_link)
