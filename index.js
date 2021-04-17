25d7ca5ef6f5fd40778499042c85fe4ea99acbd2
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-04-17 10:51:36 -0400

    unique list

--- index.js
+++ index.js
@@ -1,28 +1,78 @@
-var entrada = document.getElementById('texto-entrada').innerHTML
 
 function countWords(str) {
     return "Cantidad de Palabras: " + str.split(' ').length;
 }
 
 function ListWords(str) {
-    var palabras = str.split(' ')
+    var palabras = contar_palabras(str)
     var resultados = document.getElementById('resultados')
+    var unicos = []
     for(var i = 0; i < palabras.length ; i++){
-        var palabra = palabras[i];
-        
-        var p = document.createElement("p"); 
-        var texto_del_link = document.createTextNode(p); 
-        resultados.appendChild(texto_del_link);
+        palabra = palabras[i].palabra
+        var cant = 0
+        for(var x = 0; x < palabras.length ; x++){
+          if (palabras[x].palabra == palabra){
+            cant++
+          }
+        }
+
+        // if ya existe no hay que push
+        var sw = 0
+        for(var j = 0; j < unicos.length ; j++){
+         var unico = unicos[j]
+         if (unico.palabra == palabra) {
+            sw = 1
+            // salir de ciclo
+         }
+        }
+        if (sw == 0){
+            unicos.push(new contadorPalabra(palabra, cant))
+        }
 
     }
+
+    for(var i = 0; i < unicos.length ; i++){
+        var palabra = unicos[i];
+        
+        var strong = document.createElement('strong');
+        var texto = palabra.palabra + ":" + palabra.cantidad
+        var texto_strong = document.createTextNode(texto);
+        
+        strong.appendChild(texto_strong);
+        
+        var p = document.createElement('p');
+        p.appendChild(strong);
+        
+        resultados.appendChild(p);
+    }
+    return unicos
 }
 
-console.log (countWords(entrada))
+function contadorPalabra(palabra, cantidad=1){
+    this.palabra = palabra;
+    this.cantidad = cantidad;
+}
 
-var words_quantity = countWords(entrada)
+function contar_palabras(texto){
+    const palabras = []
+    var texto = texto.split(" ");
+    for( var i = 0 ; i < texto.length ; i++){
+        palabra = texto[i];
+        palabras.push(new contadorPalabra(palabra))
+    }
+    
+    return palabras;
+}
 
-var texto_del_link = document.createTextNode(words_quantity);
+function initilize(){
+    var entrada = document.getElementById('texto-entrada').innerHTML
 
-document.getElementById('resultados').appendChild(texto_del_link)
+    // Cantidad de Palabras
+    var words_quantity = countWords(entrada)
+    var texto_del_link = document.createTextNode(words_quantity);
+    document.getElementById('resultados').appendChild(texto_del_link)
+    // 
+    ListWords(entrada)
+}
 
-ListWords(entrada)
+initilize()
